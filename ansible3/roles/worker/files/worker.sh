6def39e17b7e8206523e97026f73a75a407c4f49
Brad Chamberlain <chamby@gmail.com>
2017-08-29 08:33:24 -0400

    added install of socat on workers

--- ansible3/roles/worker/files/worker.sh
+++ ansible3/roles/worker/files/worker.sh
@@ -7,6 +7,7 @@ sudo mv /var/tmp/ca.pem /var/lib/kubernetes/
 wget https://get.docker.com/builds/Linux/x86_64/docker-1.12.6.tgz
 tar -xvf docker-1.12.6.tgz
 sudo cp docker/docker* /usr/bin/
+sudo apt-get install socat
 
 cat > docker.service <<EOF
 [Unit]
